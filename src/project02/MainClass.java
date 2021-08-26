29b330bf7c4fc74654c28cb3e6d8062cfb6f3f10
parksunyoung <iss002@naver.com>
2021-08-26 17:50:57 +0900

    project02 create comp.

---
+++ src/project02/MainClass.java
@@ -0,0 +1,36 @@
+package project02;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int select = 0;
+		while(true) {
+			System.out.print("1.김여진");
+			System.out.print("2.박상진");
+			System.out.print("3.김형석");
+			System.out.print("4.문지은");
+			System.out.print("5.박선영");
+			
+			select = sc.nextInt();
+			
+			switch (select) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+}
