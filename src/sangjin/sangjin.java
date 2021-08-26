f08d4ceca228fd969bc4d221fcb63265866a318e
SangJin <asdfg2977@naver.com>
2021-08-26 20:57:12 +0900

    상진 완료

--- src/sangjin/sangjin.java
+++ src/sangjin/sangjin.java
@@ -1,11 +1,33 @@
 package sangjin;
 
+import java.util.Scanner;
+
 public class sangjin {
+Scanner sc = new Scanner(System.in);
+private boolean bool = true;
 public void display() {
-	System.out.println();
-	System.out.println("저는 간장 계란밥을 먹었습니다");
-	System.out.println("아주 맛이 좋더군요");
-	System.out.println("레시피는 비밀입니다.");
-	System.out.println();
+	while(bool) {
+	System.out.println("1. 출력문1    2. 출력문2    3. 나가기");
+	int num =sc.nextInt();
+	switch(num) {
+	case 1:print1();
+		break;
+	case 2:print2();
+		break;
+	case 3:exit();
+		break;
+	}
+	}
+	
+}
+public void print1() {
+	System.out.println("간장계란밥");
+}
+public void print2() {
+	System.out.println("설탕 많이");
+}
+public void exit() {
+	bool = false;
 }
 }
+	
