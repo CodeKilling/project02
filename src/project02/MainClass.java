a2cf57a14ef790f37ba569d64503ccbe81f6e126
parksunyoung <iss002@naver.com>
2021-08-26 19:28:57 +0900

    psy :: 작업 완료.

--- src/project02/MainClass.java
+++ src/project02/MainClass.java
@@ -2,17 +2,22 @@ package project02;
 
 import java.util.Scanner;
 
+import psy.TeamProject_Psy;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int select = 0;
+		
+		TeamProject_Psy tPsy = new TeamProject_Psy();
+		
 		while(true) {
 			System.out.print("1.김여진");
 			System.out.print("2.박상진");
 			System.out.print("3.김형석");
 			System.out.print("4.문지은");
-			System.out.print("5.박선영");
+			System.out.print("5.박선영\n");
 			
 			select = sc.nextInt();
 			
@@ -26,9 +31,31 @@ public class MainClass {
 			case 4:
 				break;
 			case 5:
+				boolean flag = true;
+				int n = 0;
+				
+				while(flag) {
+					tPsy.selectList();
+					n = sc.nextInt();
+					
+					switch (n) {
+					case 1:
+						tPsy.display01();
+						break;
+					case 2:
+						tPsy.display02();
+						break;
+					case 3:
+						flag = false;
+						break;
+					default:
+						System.out.println("only 1~3.");
+						break;
+					}
+				}
+				
 				break;
-			default:
-				break;
+			default: break;
 			}
 		}
 	}
---
+++ src/psy/TeamProject_Psy.java
@@ -0,0 +1,16 @@
+package psy;
+
+public class TeamProject_Psy {
+	public void display01() {
+		System.out.println("display01");
+	}
+	public void display02() {
+		System.out.println("display02");
+	}
+	
+	public void selectList() {
+		System.out.println("1. 출력문01");
+		System.out.println("2. 출력문02");
+		System.out.println("3. 나가기");
+	}
+}
