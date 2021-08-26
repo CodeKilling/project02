02642b2c61724061ee0ae662719a1686c9f87c8a
김여진 <test@t.t>
2021-08-26 21:12:17 +0900

    김여진 수정 완료

--- src/yeojin/Pj02_yj.java
+++ src/yeojin/Pj02_yj.java
@@ -5,26 +5,36 @@ import java.util.Scanner;
 public class Pj02_yj {
 
 	Scanner input = new Scanner(System.in);	
+	boolean bool = true;
 	public void display()
 	{
-		while(true)
-		{	
+		while(bool)
+		{
 			System.out.println("1.안녕 2.반갑 3.나가기");
 			int num;
 			num = input.nextInt();
 		
 			switch(num)
 			{
-			case 1 : System.out.println("하세요.");
+			case 1 : print1();
 				break;
-			case 2 : System.out.println("습니다.");
+			case 2 : print2();
 				break;
-			case 3 : 
+			case 3 : exit();
 				break;
 			}
 		}
-
 	}
-	
+	public void print1()
+	{
+		System.out.println("하세요");
+	}
+	public void print2()
+	{
+		System.out.println("습니다");
+	}
+	public void exit()
+	{
+		bool = false;
+	}
 }
-
